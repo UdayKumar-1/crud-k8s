eeb3dcad2a30f521b9cb0e5cb6095e81ceab70d9
Uday Kumar <annaldasudaykumar1@gmail.com>
2025-06-21 00:39:42 +0530

    added the input fields for login page and the crud page to implement crud operations.

--- frontend/src/components/CrudOperation.jsx
+++ frontend/src/components/CrudOperation.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
-
+import { useEffect, useState } from "react";
+import Header from "./Header";
 const CrudOperation = () => {
+  const [inputText, setInputText] = useState({
+    text: "",
+    userId: "",
+  });
+  const [allData, setAllData] = useState([]);
+  const handleOnChange = (e) => {
+    console.log("e.target.value ", e.target.value);
+    setInputText({ ...inputText, text: e.target.value });
+  };
   return (
-    <div>
-      crud page
-    </div>
-  )
-}
+    <>
+      <Header />
+      <div className="container">
+        <div className="d-flex justify-content-center my-2">
+          <input
+            type="text"
+            onChange={(e) => handleOnChange(e)}
+            className="form-control w-25"
+          />
+          <button className="btn btn-primary mx-2" onClick={()=>{}}>
+            Add
+          </button>
+        </div>
+        {typeof allData == "string" && (
+          <div className="d-flex justify-content-center">
+            There are no Tasks
+          </div>
+        )}
+        {allData.length > 0 &&
+          typeof allData != "string" &&
+          allData?.map((item, index) => (
+            <div
+              className="row g-0 w-75 py-2 rounded-3 mt-1 d-flex justify-content-center mx-4"
+              key={index}
+            >
+              <div className="col-sm-4"></div>
+              <div className="col-sm-2">
+                <span>{item.user.name}</span>
+              </div>
+              <div className="col-sm-3">
+                <span>{item.text}</span>
+              </div>
+              <div
+                className="col-sm-1 "
+                onClick={() =>{}}
+              >
+                ❌
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
 
-export default CrudOperation
+export default CrudOperation;
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import { useState } from "react";
+import Header from "./Header";
 
 const Login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const onChangeValues = (e) => {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  };
   return (
-    <div>
-      login page
-    </div>
-  )
-}
+    <>
+      <Header />
+      <div className="container mt-5 text-center w-50">
+        <form>
+          <br />
+          <input
+            type="email"
+            name="email"
+            value={user.email}
+            placeholder="Enter your email"
+            className="mainForm form-control w-50 m-auto"
+            onChange={onChangeValues}
+          />
+          <br />
+          <input
+            type="password"
+            name="password"
+            value={user.password}
+            placeholder="Enter your password"
+            className="mainForm form-control w-50 m-auto"
+            onChange={onChangeValues}
+          />
+          <br />
+          <button className="btn btn-primary">Login</button>
+        </form>
+      </div>
+    </>
+  );
+};
 
-export default Login
+export default Login;
