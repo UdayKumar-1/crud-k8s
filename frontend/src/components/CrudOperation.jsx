2d78b124de304153ec3890ce1d1bba007a3feb8b
Uday Kumar <annaldasudaykumar1@gmail.com>
2025-06-21 00:28:22 +0530

    removed unwanted files and as well as setup the registration and login crudoperations initial page setup.

---
+++ frontend/src/components/CrudOperation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CrudOperation = () => {
+  return (
+    <div>
+      crud page
+    </div>
+  )
+}
+
+export default CrudOperation
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Registration from "./components/Registration.jsx";
+import Login from "./components/Login.jsx";
+import CrudOperation from "./components/CrudOperation.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Registration />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/crud" element={<CrudOperation />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
