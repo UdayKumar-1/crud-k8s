7246d95fe8d1c538c9a308a7c83cb457c9b357ae
Uday Kumar <annaldasudaykumar1@gmail.com>
2025-06-29 22:40:17 +0530

    updated the DNS with the service names to communicate internally service to service.

--- CrudService/CrudService/src/main/java/com/crudservice/constants/URLParameters.java
+++ CrudService/CrudService/src/main/java/com/crudservice/constants/URLParameters.java
@@ -1,5 +1,5 @@
 package com.crudservice.constants;
 
 public class URLParameters {
-    public static final String USER_SERVICE_BASE_URL = "http://userservice-service.crud.svc.cluster.local:8080";
+    public static final String USER_SERVICE_BASE_URL = "http://userservice-service:8080";
 }
