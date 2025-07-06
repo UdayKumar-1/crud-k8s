dadd2a495b1d4b4e864d30b0b10b59eb0015cd1d
Uday Kumar <annaldasudaykumar1@gmail.com>
2025-07-06 22:55:27 +0530

    deleted 2 unwanted files from crud application.

--- CrudService/CrudService/src/main/java/com/crudservice/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.crudservice.service;
-
-import com.crudservice.dto.UserDto;
-
-public interface UserService {
-    UserDto userRegistrationService(UserDto user);
-    UserDto userLoginService(String name,String password);
-    UserDto getUserRecordService(Long id);
-}
